6cc94b36e3e6e94a964061a718e601dac2190aeb
Daniel-171717 <danielhipo770@gmail.com>
2025-01-26 17:57:00 -0500

    Add files via upload

---
+++ PryNotas/src/main/java/com/mycompany/prynotas/PryNotas.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.prynotas;
+
+import ViewNotas.ViewNotas1;
+
+public class PryNotas {
+
+    public static void main(String[] args) {
+        ViewNotas1 vista = new ViewNotas1();
+        vista.setVisible(true);
+                
+    }
+}
